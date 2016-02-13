2ac803cbfb0eda8b849c67dd3c0907a676d14c01
ST <support@neuralfutures.com>
2016-02-13 15:35:38 +0000

    More Router/Dealer examples.

--- NetMQ.ReactiveExtensions.Tests/RouterDealerTest2.cs
+++ NetMQ.ReactiveExtensions.Tests/RouterDealerTest2.cs
@@ -29,8 +29,12 @@ namespace NetMQ.ReactiveExtensions.Tests
 
 			var clientSocketPerThread = new ThreadLocal<DealerSocket>();
 
-			using (var server = new RouterSocket("@tcp://127.0.0.1:5556"))
+			string endpoint;
+
+			using (var server = new RouterSocket("@tcp://127.0.0.1:0")) // If we specify 0, it will choose a random port for us.
 			{
+				endpoint = server.Options.LastEndpoint; // Lets us know which port was chosen.
+				Console.Write("Last endpoint, including port: {0}\n", server.Options.LastEndpoint);
 				using (var poller = new NetMQPoller())
 				{
 					// Start some threads, each with its own DealerSocket
@@ -48,7 +52,7 @@ namespace NetMQ.ReactiveExtensions.Tests
 								client = new DealerSocket();
 								client.Options.Identity =
 									Encoding.Unicode.GetBytes(state.ToString());
-								client.Connect("tcp://127.0.0.1:5556");
+								client.Connect(endpoint);
 								//client.ReceiveReady += Client_ReceiveReady;
 								clientSocketPerThread.Value = client;
 								poller.Add(client);
@@ -58,8 +62,16 @@ namespace NetMQ.ReactiveExtensions.Tests
 								client = clientSocketPerThread.Value;
 							}
 
+							Thread.Sleep(3000); // Wait until server is up.
+							client.SendFrame("Ping");
+
 							while (true)
 							{
+								Console.Write("Client {0}: Waiting for ping...\n", i);
+								// Work around "feature" of router/dealer: the publisher does not know the subscriber exists, until it
+								// sends at least one message which makes it necessary to open the connection. I believe this is a
+								// low-level feature of the TCP/IP transport.
+
 								var clientMessage = client.ReceiveMultipartMessage();
 								Console.WriteLine("======================================");
 								Console.WriteLine(" INCOMING CLIENT MESSAGE FROM SERVER");
@@ -77,7 +89,7 @@ namespace NetMQ.ReactiveExtensions.Tests
 
 					// server loop
 					int sequenceNo = 0;
-					for (int i=0;i<5;i++)
+					for (int i=0;i<10;i++)
 					{
 						NetMQMessage messageToServer = new NetMQMessage();
 						messageToServer.AppendEmptyFrame();
@@ -90,6 +102,7 @@ namespace NetMQ.ReactiveExtensions.Tests
 						server.SendMultipartMessage(messageToServer);
 						Thread.Sleep(delay);						
 					}
+
 					Console.WriteLine("Finished.");
 				}
 			}
@@ -116,30 +129,30 @@ namespace NetMQ.ReactiveExtensions.Tests
 		}
 
 		[Test]
-		public void Two_Messages_FromRouter_To_Dealer()
+		public void Two_Messages_From_Dealer_To_Router()
 		{
-			using (var server = new RouterSocket())
-			using (var client = new DealerSocket())
-			using (var poller = new NetMQPoller { client })
+			int maxMessage = 5;
+			CountdownEvent cd = new CountdownEvent(maxMessage);
+
+			Console.Write("Test sending message from subscribers (dealer) to publisher(router).\n");
+
+			using (var publisher = new RouterSocket())
+			using (var subscriber = new DealerSocket())
+			using (var poller = new NetMQPoller { subscriber })
 			{
-				var port = server.BindRandomPort("tcp://*");
-				client.Connect("tcp://127.0.0.1:" + port);
-				var cnt = 0;
-				client.ReceiveReady += (sender, e) =>
+				var port = publisher.BindRandomPort("tcp://*");
+				subscriber.Connect("tcp://127.0.0.1:" + port);
+				subscriber.ReceiveReady += (sender, e) =>
 				{
 					var strs = e.Socket.ReceiveMultipartStrings();
 					foreach (var str in strs)
 					{
 						Console.WriteLine(str);
 					}
-					cnt++;
-					if (cnt == 2)
-					{
-						poller.Stop();
-					}
+					cd.Signal();
 				};
 				byte[] clientId = Encoding.Unicode.GetBytes("ClientId");
-				client.Options.Identity = clientId;
+				subscriber.Options.Identity = clientId;
 
 				const string request = "GET /\r\n";
 
@@ -148,16 +161,74 @@ namespace NetMQ.ReactiveExtensions.Tests
 						"\r\n" +
 						"Hello, World!";
 
-				client.SendFrame(request);
+				subscriber.SendFrame(request);
 
-				byte[] serverId = server.ReceiveFrameBytes();
-				Assert.AreEqual(request, server.ReceiveFrameString());
+				byte[] serverId = publisher.ReceiveFrameBytes();
+				Assert.AreEqual(request, publisher.ReceiveFrameString());
 
-				// two messages in a row, not frames
-				server.SendMoreFrame(serverId).SendFrame(response);
-				server.SendMoreFrame(serverId).SendFrame(response);
+				for (int i = 0; i < maxMessage; i++)
+				{
+					publisher.SendMoreFrame(serverId).SendFrame(response);
+				}
 
-				poller.Run();
+				poller.RunAsync();
+
+				if (cd.Wait(TimeSpan.FromSeconds(10)) == false) // Blocks until _countdown.Signal has been called.
+				{
+					Assert.Fail("Timed out, this test should complete in less than 10 seconds.");
+				}
+			}
+		}
+
+		[Test]
+		public void Two_Messages_From_Router_To_Dealer()
+		{
+			Console.Write("Test sending message from publisher(router) to subscribers (dealer).\n");
+
+			int maxMessage = 5;
+			CountdownEvent cd = new CountdownEvent(maxMessage);
+
+			using (var publisher = new RouterSocket()) // Publisher.
+			using (var subscriber = new DealerSocket())
+			using (var poller = new NetMQPoller { subscriber })
+			{
+				var port = publisher.BindRandomPort("tcp://*");
+				subscriber.Connect("tcp://127.0.0.1:" + port);
+				subscriber.ReceiveReady += (sender, e) =>
+				{
+					var strs = e.Socket.ReceiveMultipartStrings();
+					foreach (var str in strs)
+					{
+						Console.WriteLine("Subscribe: " + str);
+					}
+					cd.Signal();
+				};
+				byte[] clientId = Encoding.Unicode.GetBytes("ClientId");
+				subscriber.Options.Identity = clientId;
+
+				const string request = "Ping";
+
+				// Work around "feature" of router/dealer: the publisher does not know the subscriber exists, until it
+                // sends at least one message which makes it necessary to open the connection. I believe this is a
+                // low-level feature of the TCP/IP transport.
+				subscriber.SendFrame(request); // Ping.
+
+				byte[] serverId = publisher.ReceiveFrameBytes();
+				Assert.AreEqual(request, publisher.ReceiveFrameString());
+
+				for (int i = 0; i < maxMessage; i++)
+				{
+					string msg = string.Format("[message: {0}]", i);
+					Console.Write("Publish: {0}\n", msg);
+					publisher.SendMoreFrame(serverId).SendFrame(msg);
+				}
+
+				poller.RunAsync();
+
+				if (cd.Wait(TimeSpan.FromSeconds(10)) == false) // Blocks until _countdown.Signal has been called.
+				{
+					Assert.Fail("Timed out, this test should complete in less than 10 seconds.");
+				}
 			}
 		}
 	}
