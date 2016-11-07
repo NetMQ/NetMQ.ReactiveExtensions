327b1f2d7e4fd187fa2aebc720cda44f4c793d1c
Gravitas <support@designrem.com>
2016-11-07 21:06:04 +0000

    One more issue before compatibility with .NET Core 1.0.

---
+++ NetMQ.ReactiveExtensions.Tests/SerializeExceptionToXml_Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using NUnit.Framework;
+
+// ReSharper disable InconsistentNaming
+
+namespace NetMQ.ReactiveExtensions.Tests
+{
+    public class SerializeExceptionToXml_Tests
+    {
+
+        [TestFixture]
+        public static class Serialize_Exception_To_Xml_Tests
+        {
+            [Test]
+            public static void Convert_Exception_To_XML()
+            {
+                ApplicationException outerException = new ApplicationException();
+                ExceptionXElement xmlRaw;
+                {
+                    try
+                    {
+                        var path = File.ReadAllLines("file does not exist");
+                    }
+                    catch (Exception ex)
+                    {
+                        outerException = new ApplicationException("Outer exception", ex);
+                    }
+
+                    xmlRaw = new ExceptionXElement(outerException);
+                }
+
+                var xml = xmlRaw.ToString();    
+                          
+                Assert.IsTrue(xml.Contains("ApplicationException"));
+                Assert.IsTrue(xml.Contains("<"));
+                Assert.IsTrue(xml.Contains(">"));
+            }
+        }
+    }
+}
+
