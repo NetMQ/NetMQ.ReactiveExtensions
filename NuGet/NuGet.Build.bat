19a6ca8c09bdc0f8ad2a5708ac88e673ca966803
Gravitas <support@designrem.com>
2016-11-09 21:40:29 +0000

    Updating release version.

--- NuGet/NuGet.Build.bat
+++ NuGet/NuGet.Build.bat
@@ -5,12 +5,14 @@ echo Hint 1: Have you used Visual Studio to build the "Release / AnyCPU" version
 echo.
 echo Hint 2: Do we have the latest version of "nuget.exe"?
 echo.
-dir .\bin\Release\*.*
+dir ..\NetMQ.ReactiveExtensions\bin\Release\net45\*.*
+dir ..\NetMQ.ReactiveExtensions\bin\Release\netcoreapp1.0\*.*
+dir ..\NetMQ.ReactiveExtensions\bin\Release\netstandard1.6\*.*
 echo.
 echo Hint 3: Is the "Release" build above recent?
 echo.
 pause
-set version=0.9.4-rc5
+set version=0.9.4-rc6
 nuget.alpha pack NetMQ.ReactiveExtensions.nuspec -Version %version%
 rem TODO: Figure out why the new version is giving that error - hopefully they will release a new alpha that fixes the issue.
 nuget push NetMQ.ReactiveExtensions.%version%.nupkg 9871c41e-c402-4e85-8c96-5486fb725868
